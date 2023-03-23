8528a03b382d796b200eac2da5a0df567a4c5ff7
jani <jani.behm@gmail.com>
2023-03-23 07:18:42 +0200

    mobile nav added

---
+++ script.js
@@ -0,0 +1,55 @@
+const backButton = document.querySelector('#backToTop');
+const header = document.querySelector('header');
+const mobButton = document.querySelector('.mobile');
+const nav = document.querySelector('nav ul')
+const menuItems = document.querySelectorAll('nav ul li a')
+const modalButton = document.querySelector('#modalButton')
+const overlay = document.querySelector('.overlay')
+const closeButton = document.querySelector('.close')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, smoothly scroll to the top of the document
+function topFunction() {
+  // Get the current scroll position
+  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  
+  // If the current scroll position is greater than 0, scroll to top smoothly
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(topFunction);
+    window.scrollTo(0, currentScroll - (currentScroll / 10));
+  }
+}
+
+// When the user clicks on the "Contact" link, smoothly scroll to the bottom of the page
+function scrollToBottom() {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+}
+
+const mobMenu = () => {
+    for (const link of menuItems) {
+        link.addEventListener('click', mobMenu)
+    }
+
+    if (nav.classList.contains('responsive')) {
+        nav.classList.remove('responsive')
+    } else {
+        nav.classList.add('responsive')
+    }
+}
+
+const modalShow = () => {
+    overlay.classList.toggle('visible')
+}
+
+mobButton.addEventListener('click', mobMenu)
+
